6f79cdcf050b2cc37cf47e1b116acbe5ddb9039b
AfreenZehra1512 <afreen.zehra1512@gmail.com>
2025-02-24 20:23:54 +0100

    Modified configs, and added model package

---
+++ src/main/java/com/webshop/model/Cart_Item.java
@@ -0,0 +1,22 @@
+package com.webshop.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart_Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private int quantity;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
---
+++ src/main/java/com/webshop/model/Order.java
@@ -0,0 +1,24 @@
+package com.webshop.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String date;
+    private String status;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<Cart_Item> items;
+}
