f0306f7b9b25fefbf5ee91aa3ef4a87303cdab21
AfreenZehra1512 <afreen.zehra1512@gmail.com>
2025-02-24 20:05:45 +0100

    Initial commit

---
+++ src/main/java/com/webshop/Cloud_Computing_Webshop_Backend/CloudComputingWebshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.Cloud_Computing_Webshop_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudComputingWebshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudComputingWebshopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webshop/Cloud_Computing_Webshop_Backend/CloudComputingWebshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.Cloud_Computing_Webshop_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudComputingWebshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
